f597eddb1d08a40382e195ab157c39d483a67bdb
Satyan Mumbarkar <61200623+sattumac@users.noreply.github.com>
2020-07-04 10:49:21 +0530

    Add files via upload

---
+++ TakeHomeExercise1.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Take Home Exercise 1
+
+# In[1]:
+
+
+#States In India
+States = ['Maharashtra',
+ 'Tamil Nadu',
+ 'Delhi',
+ 'Gujarat',
+ 'Uttar Pradesh',
+ 'West Bengal',
+ 'Rajasthan',
+ 'Telengana',
+ 'Karnataka',
+ 'Andhra Pradesh',
+ 'Haryana',
+ 'Madhya Pradesh',
+ 'Bihar',
+ 'Assam',
+ 'Jammu and Kashmir',
+ 'Odisha',
+ 'Punjab',
+ 'Kerala',
+ 'Uttarakhand',
+ 'Chhattisgarh',
+ 'Jharkhand',
+ 'Tripura',
+ 'Goa',
+ 'Manipur',
+ 'Ladakh',
+ 'Himachal Pradesh',
+ 'Puducherry',
+ 'Nagaland',
+ 'Chandigarh',
+ 'Dadra and Nagar Haveli',
+ 'Arunachal Pradesh',
+ 'Mizoram',
+ 'Sikkim',
+ 'Andaman and Nicobar',
+ 'Meghalaya',
+ 'Lakshadweep']
+States
+
+
+# In[ ]:
+
+
+#Infected Person As Respective of States
+Infected = [180298,
+ 94049,
+ 89802,
+ 33232,
+ 24056,
+ 19170,
+ 18312,
+ 17357,
+ 16514,
+ 15252,
+ 14941,
+ 13861,
+ 10249,
+ 8582,
+ 7695,
+ 7316,
+ 5668,
+ 4593,
+ 2947,
+ 2940,
+ 2521,
+ 1396,
+ 1387,
+ 1260,
+ 990,
+ 979,
+ 714,
+ 459,
+ 446,
+ 215,
+ 195,
+ 160,
+ 101,
+ 100,
+ 52,
+ 0]
+Infected
+
+
+# In[2]:
+
+
+#Data in Dictionary
+CovidData = {'Maharashtra': 180298,
+ 'Tamil Nadu': 94049,
+ 'Delhi': 89802,
+ 'Gujarat': 33232,
+ 'Uttar Pradesh': 24056,
+ 'West Bengal': 19170,
+ 'Rajasthan': 18312,
+ 'Telengana': 17357,
+ 'Karnataka': 16514,
+ 'Andhra Pradesh': 15252,
+ 'Haryana': 14941,
+ 'Madhya Pradesh': 13861,
+ 'Bihar': 10249,
+ 'Assam': 8582,
+ 'Jammu and Kashmir': 7695,
+ 'Odisha': 7316,
+ 'Punjab': 5668,
+ 'Kerala': 4593,
+ 'Uttarakhand': 2947,
+ 'Chhattisgarh': 2940,
+ 'Jharkhand': 2521,
+ 'Tripura': 1396,
+ 'Goa': 1387,
+ 'Manipur': 1260,
+ 'Ladakh': 990,
+ 'Himachal Pradesh': 979,
+ 'Puducherry': 714,
+ 'Nagaland': 459,
+ 'Chandigarh': 446,
+ 'Dadra and Nagar Haveli': 215,
+ 'Arunachal Pradesh': 195,
+ 'Mizoram': 160,
+ 'Sikkim': 101,
+ 'Andaman and Nicobar': 100,
+ 'Meghalaya': 52,
+ 'Lakshadweep': 0}
+CovidData
+
+
+# In[ ]:
+
+
+
+
