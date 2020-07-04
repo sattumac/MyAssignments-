f0f01b00a2f7afaf2bd4bec9f6e84c58a6c0c4b3
Satyan Mumbarkar <61200623+sattumac@users.noreply.github.com>
2020-07-04 10:43:43 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Change in the infected in given Three days 
+
+# In[25]:
+
+
+#Name of District 
+District = "Gwalior"
+#Total Infected
+Total_Infected = 1234
+#Record of Three Days
+tenthjune = 12
+eleventhjune = 2
+twelfthjune = 13
+#Percentage Calculation
+percent= ((twelfthjune-tenthjune)/twelfthjune)*100
+#Print Record
+print("In",District,percent,"% of Corona Patients are Increased")
+
